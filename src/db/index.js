44d8e240fb5cc514ced071b7c87ed2b788c52dea
Sunny <sd2614496@gmail.com>
2025-07-12 15:46:49 +0530

    util code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import app from "../app.js";
+
 
 const connectDB = async () =>{
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("ERROR:", ()=>{
+            console.log("ERROR:",error);
+            throw error
+        })
         console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
     } catch (error){
         console.log("MONGODB connection Failed", error);
